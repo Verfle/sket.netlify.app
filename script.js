4c82bf18497953da92e7d5712d6442b89577dccf
Nothing here <96931728+Nuskfolio@users.noreply.github.com>
2022-03-01 10:13:53 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-function facebook() {
-    var copyText = document.getElementById("facebook");
-    copyText.select();
-    copyText.setSelectionRange(0, 99999);
-    navigator.clipboard.writeText(copyText.value);
-    Swal.fire(
-    'The Internet?',
-    'That thing is still around?',
-    'question'
-    )
-}
+// Some JavaScript
